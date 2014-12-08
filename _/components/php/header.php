be436bdfdbfe49c18c7112c96c19cde64178acf0
Eric Sartorius <Epic@Erics-MacBook-Pro.local>
2014-12-08 15:36:11 +0900

    header and index added

---
+++ _/components/php/header.php
@@ -0,0 +1,20 @@
+<div class="content row">
+	<div class="col-lg-12">
+		<header class="clearfix">
+			<section id="branding">
+		<!--		<a href="index.php"><img src="****" alt="Seoul Seekers logo"></a>   -->
+			</section><!-- branding -->
+
+			<section class="navbar">
+				<ul class="nav navbar-nav">
+					<li><a href="index.php">Home</a></li>
+					<li><a href="about.php">About</a></li>
+					<li><a href="blog.php">Blog</a></li>
+					<li><a href="events.php">Events</a></li>
+					<li><a href="booking.php">Booking</a></li>
+				</ul><!-- nav -->
+			</section><!-- navbar -->
+
+		</header><!-- header -->
+	</div><!-- column -->
+</div><!-- content -->
