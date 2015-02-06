5ab13886de6c03dfb8a2fe49d813b41b4acf967b
EricSSartorius <EricSSartorius@gmail.com>
2015-02-06 20:58:45 +0900

    minor styling adjustments

--- _/components/php/article-bookingform.php
+++ _/components/php/article-bookingform.php
@@ -1,28 +1,28 @@
 <article class="bookingform">
-	<h2>Booking</h2>
+	<h2>BOOKING</h2>
 	<p>The Seoul Seekers can be booked for performances, TV shows, and modeling gigs. For enquires please complete the form below.</p>
 	<p>(For individual booking please include name of member desired in the message box)</p>
 <br/>
 	<form class="registration form-horizontal" action="#">
 
 	  <fieldset id="personalinfo">
-	    <legend>Personal Info</legend>
+	    <legend class="personal-type"><h3>Personal Info</h3></legend>
 		
-				<section class="row">
+				<section class="row booking-row">
 					<label class="col col-lg-4 control-label" for="myname">Name:<span class="required">*</span></label></label>
 					<div class="controls">
 						<input class="col col-lg-8" type="text" name="myname" id="myname" required>
 					</div><!-- controls -->
 				</section><!-- row -->
 
-				<section class="row">
+				<section class="row booking-row">
 					<label class="col col-lg-4 control-label" for="companyname">Company Name: </label>
 					<div class="controls">
 						<input class="col col-lg-8" type="text" name="companyname" id="companyname" />
 					</div><!-- controls -->
 				</section><!-- row -->
 
-				<section class="row">
+				<section class="row booking-row">
 					<label class="col col-lg-4 control-label" for="myemail">Email:<span class="required">*</span></label></label>
 					<div class="controls">
 						<input class="col col-lg-8" type="email" name="myemail" id="myemail" required autocomplete="off" />
@@ -32,7 +32,7 @@
 	  </fieldset><!-- personal info -->
 
 	  <fieldset id="otherinfo">
-			<legend>Booking Type</legend>
+			<legend class="booking-type"><h3>Booking Type</h3></legend>
 
 				<section class="row">
 		  	<label class="col col-lg-4 control-label"></label>
@@ -50,7 +50,7 @@
 					</div><!-- controls -->
 				</section><!-- row -->
 	<p></p>
-				<section class="row">
+				<section class="row booking-row">
 					<label class="col col-lg-4 control-label" for="message">Message:<span class="required">*</span></label>
 					<div class="controls">
 					<textarea cols="50" rows="4" class= "col col-lg-8" id="message" name="message" placeholder="Your message here" required></textarea>
--- _/components/php/article-gallery.php
+++ _/components/php/article-gallery.php
@@ -1,8 +1,8 @@
-<article class="gallery-pictures">
-  <h2>Seoul Seekers Picture Gallery</h2>
+<article class="gallery">
+  <h2>PICTURE GALLERY</h2>
   <p>Lorasdfasdfasdfasem ipsum dolor sit amet, consectetur adipisicing elit. Maxime dolorem possimus quam consequatur, dolor doloribus nam voluptatibus molestias suscipit facilis ab itaque sunt nobis harum laudantium porro dignissimos neque reprehenderit! Lorem ipsum dolor sit amet, consectetur adipisicing elit. Delectus illo cum voluptas, aliquam, dolorem obcaecati ipsum nemo magnam aut. Quidem quos in culpa, quam harum omnis nemo laborum aliquid voluptates.</p>
   <p class="hidden-sm">Lorem ipsum dolor sit amet, consectetur adipisicing elit. Nisi, consequuntur aspernatur non cupiditate in quidem maxime hic minima vitae quos maiores fuga atque reprehenderit obcaecati suscipit est unde nemo, eaque. Lorem ipsum dolor sit amet, consectetur adipisicing elit. Blanditiis esse maxime veniam quod earum quaerat amet, repudiandae error. Voluptate natus reprehenderit, ipsum eligendi possimus illum saepe nihil ex animi soluta?</p>
-	<div class="modalphotos photogrid clearfix">
+	<div class="gallery-pictures modalphotos photogrid clearfix text-center">
 		<img src="images/slides/SScrew1_tn.jpg" alt="SS Crew 1">
 		<img src="images/slides/SScrew2_tn.jpg" alt="SS Crew 2">
 		<img src="images/slides/SSjacket_tn.jpg" alt="SS Jacket">
