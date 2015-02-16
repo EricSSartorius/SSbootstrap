a2ff6da899112d4c7e91a2065e0fd90037013677
EricSSartorius <EricSSartorius@gmail.com>
2015-02-16 12:09:12 +0900

    updated social icons on intro page

--- _/components/php/article-intro.php
+++ _/components/php/article-intro.php
@@ -1,64 +1,33 @@
+
+
 <article class="intro">
- 	<h2>SS Intro</h2>
-	<p>Lorem ipsum dolor sit amet, consectetur adipisicing elit. Maxime dolorem possimus quam consequatur, dolor doloribus nam voluptatibus molestias suscipit facilis ab itaque sunt nobis harum laudantium porro dignissimos neque reprehenderit! Lorem ipsum dolor sit amet, consectetur adipisicing elit. Delectus illo cum voluptas, aliquam, dolorem obcaecati ipsum nemo magnam aut. Quidem quos in culpa, quam harum omnis nemo laborum aliquid voluptates.</p>
-	<p class="hidden-sm">Lorem ipsum dolor sit amet, consectetur adipisicing elit. Nisi, consequuntur aspernatur non cupiditate in quidem maxime hic minima vitae quos maiores fuga atque reprehenderit obcaecati suscipit est unde nemo, eaque. Lorem ipsum dolor sit amet, consectetur adipisicing elit. Blanditiis esse maxime veniam quod earum quaerat amet, repudiandae error. Voluptate natus reprehenderit, ipsum eligendi possimus illum saepe nihil ex animi soluta?</p>
-		<ul class="intro-social list-inline social-buttons text-center">
-            <li>
-                <span class="fa-stack fa-4x">
-                    <a href="https://twitter.com/seoulseekers"><i class="fa fa-twitter"></i></a>
-                </span>
-            </li>
-            <li>
-                <span class="fa-stack fa-4x">
-                    <a href="https://www.facebook.com/the.seoul.seekers"><i class="fa fa-facebook"></i></a>
-                </span>
-            </li>
-            <li>
-                <span class="fa-stack fa-4x">
-                    <a href="#"><i class="fa fa-youtube"></i></a>
-                </span>
-            </li>
-        </ul>
-
-</article> <!-- intro -->
-
-
-
-<!--
- <section id="services">
+    <section class="list-inline social-buttons">
         <div class="container">
             <div class="row">
-                <div class="col-lg-12 text-center">
-                    <h2 class="section-heading">Services</h2>
+                <div class="col-lg-12">
+                    <h2 class="section-heading">SS Intro</h2>
                     <h3 class="section-subheading text-muted">Lorem ipsum dolor sit amet consectetur.</h3>
+                    <p>Lorem ipsum dolor sit amet, consectetur adipisicing elit. Maxime dolorem possimus quam consequatur, dolor doloribus nam voluptatibus molestias suscipit facilis ab itaque sunt nobis harum laudantium porro dignissimos neque reprehenderit! Lorem ipsum dolor sit amet, consectetur adipisicing elit. Delectus illo cum voluptas, aliquam, dolorem obcaecati ipsum nemo magnam aut. Quidem quos in culpa, quam harum omnis nemo laborum aliquid voluptates.</p>
+                    <p class="hidden-sm">Lorem ipsum dolor sit amet, consectetur adipisicing elit. Nisi, consequuntur aspernatur non cupiditate in quidem maxime hic minima vitae quos maiores fuga atque reprehenderit obcaecati suscipit est unde nemo, eaque. Lorem ipsum dolor sit amet, consectetur adipisicing elit. Blanditiis esse maxime veniam quod earum quaerat amet, repudiandae error. Voluptate natus reprehenderit, ipsum eligendi possimus illum saepe nihil ex animi soluta?</p>
                 </div>
             </div>
             <div class="row text-center">
-                <div class="col-md-4">
+                <div class="col col-lg-4">
                     <span class="fa-stack fa-4x">
-                        <i class="fa fa-circle fa-stack-2x text-primary"></i>
-                        <i class="fa fa-shopping-cart fa-stack-1x fa-inverse"></i>
+                         <a href="https://twitter.com/seoulseekers"><i class="fa intro-social fa-twitter"></i></a>
                     </span>
-                    <h4 class="service-heading">E-Commerce</h4>
-                    <p class="text-muted">Lorem ipsum dolor sit amet, consectetur adipisicing elit. Minima maxime quam architecto quo inventore harum ex magni, dicta impedit.</p>
                 </div>
-                <div class="col-md-4">
+                <div class="col  col-lg-4">
                     <span class="fa-stack fa-4x">
-                        <i class="fa fa-circle fa-stack-2x text-primary"></i>
-                        <i class="fa fa-laptop fa-stack-1x fa-inverse"></i>
+                        <a href="https://www.facebook.com/the.seoul.seekers"><i class="fa intro-social fa-facebook"></i></a>
                     </span>
-                    <h4 class="service-heading">Responsive Design</h4>
-                    <p class="text-muted">Lorem ipsum dolor sit amet, consectetur adipisicing elit. Minima maxime quam architecto quo inventore harum ex magni, dicta impedit.</p>
                 </div>
-                <div class="col-md-4">
+                <div class="col  col-lg-4">
                     <span class="fa-stack fa-4x">
-                        <i class="fa fa-circle fa-stack-2x text-primary"></i>
-                        <i class="fa fa-lock fa-stack-1x fa-inverse"></i>
+                         <a href="#"><i class="fa intro-social fa-youtube"></i></a>
                     </span>
-                    <h4 class="service-heading">Web Security</h4>
-                    <p class="text-muted">Lorem ipsum dolor sit amet, consectetur adipisicing elit. Minima maxime quam architecto quo inventore harum ex magni, dicta impedit.</p>
                 </div>
             </div>
         </div>
     </section>
- -->
+</article>
